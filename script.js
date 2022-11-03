773a3365a0b63583332afab19bc744b75b1c690e
Alex-cet <116481552+Alex-cet@users.noreply.github.com>
2022-11-03 22:34:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var words = [];
+let words = [];
 function addWords() {
     words.push(document.getElementById("inputWord").value);
     document.getElementById("inputWord").value = '';
 }
 function search() {
-    var wordToSearch = document.getElementById("wordToSearch").value;
+    let wordToSearch = document.getElementById("wordToSearch").value;
     if (words.indexOf(wordToSearch) > -1) {
-        alert("The word " + "'" + wordToSearch + "'" + " is in the dictionary!");
+        document.getElementById("verifyExistance").innerHTML = "The word: " + "'" +  wordToSearch + "'" + " is in the dictionary!";
     } else {
-        alert("The word " + "'" + wordToSearch + "'" + " is not in the dictionary!");
+        document.getElementById("verifyExistance").innerHTML = "The word: " + "'" +  wordToSearch + "'" + " is not in the dictionary!";
     }
     document.getElementById("wordToSearch").value = '';
 }
